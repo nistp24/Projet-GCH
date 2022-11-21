779d0f9c426833f35608c6adacc156fa25176269
LSChiasson <118693748+LSChiasson@users.noreply.github.com>
2022-11-21 14:46:13 +0000

    position

--- Fonctions.py
+++ Fonctions.py
@@ -1,17 +1,34 @@
+import numpy as np
+def position(X,Y,nx,ny):
+    """ Fonction générant deux matrices de discrétisation de l'espace
 
-Louis = 69
-Palin = -8
+    Entrées:
+        - X : Bornes du domaine en x, X = [x_min, x_max]
+        - Y : Bornes du domaine en y, Y = [y_min, y_max]
+        - nx : Discrétisation de l'espace en x (nombre de points)
+        - ny : Discrétisation de l'espace en y (nombre de points)
+    Sorties (dans l'ordre énuméré ci-bas):
+        - x : Matrice (array) de dimension (ny x nx) qui contient la position en x
+        - y : Matrice (array) de dimension (ny x nx) qui contient la position en y
+    """
+    x = np.zeros([ny,nx])
+    y = np.zeros([ny,nx])
+    dx = np.linspace(X[0],X[1], nx)
+    dy = np.linspace(Y[1],Y[0], ny)
+    
+    for i in range(ny):
+        for j in range(len(dx)):
+            x[i,j] = dx[j]    
+    for j in range(ny):
+        for k in range(nx):
+            y[j,k] = dy[j]
 
-GAY = Palin + Louis
+    return x, y
 
-print(GAY)
 
-#deuxieme test pour voir si palin est encore gay
-
-trèsGay = GAY**2
-
-print(trèsGay)
-
-#ou est palin
-rep = 'chez solene'
-print(rep)
+X = [0,8]
+Y = [0,10]
+nx = 8
+ny = 10
+posx,posy = position(X,Y,nx,ny)
+print(posx,posy)
